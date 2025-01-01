b785e2f1f358277f89a4df90a42af1d251ce425a
Nicolas Eiriz <eiriznicolas@gmail.com>
2025-01-01 17:58:25 -0300

    modifico

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ function validarFormulario(event){
     $form.className = 'oculto'
     document.querySelector('#exito').className = ''
     setTimeout(()=>{
-      window.location.href = '/wishlist.html'
+      window.location.href = '/validadorFormulario/wishlist.html'
     },5000)
     
   }
